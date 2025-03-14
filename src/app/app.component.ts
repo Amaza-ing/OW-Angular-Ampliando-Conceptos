5f04f277fd5db0f0a3c10533b6180aa486a11749
Adrián Maza <adri_mv1@hotmail.com>
2025-03-14 19:01:25 +0100

    routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { RecommendationsBannerComponent } from './components/recommendations-banner/recommendations-banner.component';
 import { NgClass } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, RecommendationsBannerComponent, NgClass],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    RecommendationsBannerComponent,
+    NgClass,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
