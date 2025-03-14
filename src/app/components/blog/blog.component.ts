df441e41633fed0fbcbc652885c19b0221175ffa
Adrián Maza <adri_mv1@hotmail.com>
2025-03-14 18:36:38 +0100

    ngStyle

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { ArticleCardComponent } from '../article-card/article-card.component';
-import { Article } from '../../models/Article';
 import { ArticleService } from '../../services/article.service';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-blog',
-  imports: [ArticleCardComponent],
+  imports: [ArticleCardComponent, NgStyle],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css',
 })
 export class BlogComponent {
+  titleSize = 30;
+
   constructor(public articleService: ArticleService) {}
 
   deleteArticle(id: number) {
@@ -17,5 +19,11 @@ export class BlogComponent {
       (article) => article.id === id
     );
     this.articleService.articles.splice(index, 1);
+  }
+
+  changeTitleSize(value: number) {
+    this.titleSize += value;
+
+    this.titleSize = Math.min(40, Math.max(20, this.titleSize));
   }
 }
