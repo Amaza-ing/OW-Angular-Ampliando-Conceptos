21405088d803150f39548a384f36bb1779a9833d
Adrián Maza <adri_mv1@hotmail.com>
2025-03-14 19:40:28 +0100

    parametric routes

--- src/app/components/article-card/article-card.component.ts
+++ src/app/components/article-card/article-card.component.ts
@@ -1,9 +1,10 @@
 import { Component, input, output, signal } from '@angular/core';
 import { Article } from '../../models/Article';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-article-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './article-card.component.html',
   styleUrl: './article-card.component.css',
 })
