41a95b97694dbf7a190f7b233b0068955f28537a
Adrián Maza <adri_mv1@hotmail.com>
2025-03-14 13:59:54 +0100

    input

--- src/app/components/article-card/article-card.component.ts
+++ src/app/components/article-card/article-card.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Article } from '../../models/Article';
 
 @Component({
   selector: 'app-article-card',
   imports: [],
   templateUrl: './article-card.component.html',
-  styleUrl: './article-card.component.css'
+  styleUrl: './article-card.component.css',
 })
 export class ArticleCardComponent {
-
+  @Input()
+  article?: Article;
 }
--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
-import { ArticleCardComponent } from "../article-card/article-card.component";
+import { ArticleCardComponent } from '../article-card/article-card.component';
+import { Article } from '../../models/Article';
 
 @Component({
   selector: 'app-blog',
   imports: [ArticleCardComponent],
   templateUrl: './blog.component.html',
-  styleUrl: './blog.component.css'
+  styleUrl: './blog.component.css',
 })
 export class BlogComponent {
-
+  articles: Article[] = [
+    {
+      id: 1,
+      title: 'Título 1',
+      body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae rem sit, ea nulla iusto fugiat veniam sapiente impedit optio accusantium necessitatibus natus nihil fuga qui officiis amet eos harum. Similique.',
+      author: 'alberto@email.com',
+    },
+    {
+      id: 2,
+      title: 'Título 2',
+      body: 'Beatae rem sit, ea nulla iusto fugiat veniam sapiente impedit optio accusantium necessitatibus natus nihil fuga qui officiis amet eos harum. Similique.',
+      author: 'beatriz@email.com',
+    },
+    {
+      id: 3,
+      title: 'Título 3',
+      body: 'Optio accusantium necessitatibus natus nihil fuga qui officiis amet eos harum. Similique.',
+      author: 'carlos@email.com',
+    },
+  ];
 }
