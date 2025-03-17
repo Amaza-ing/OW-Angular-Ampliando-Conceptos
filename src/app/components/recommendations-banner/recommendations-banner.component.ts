6354f19487e2fc071e7ad7f74a20ef6461308399
Adrián Maza <adri_mv1@hotmail.com>
2025-03-17 16:46:30 +0100

    material component

--- src/app/components/recommendations-banner/recommendations-banner.component.ts
+++ src/app/components/recommendations-banner/recommendations-banner.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ArticleService } from '../../services/article.service';
 import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-recommendations-banner',
-  imports: [RouterLink],
+  imports: [RouterLink, MatButtonModule],
   templateUrl: './recommendations-banner.component.html',
-  styleUrl: './recommendations-banner.component.css'
+  styleUrl: './recommendations-banner.component.css',
 })
 export class RecommendationsBannerComponent {
   constructor(public articleService: ArticleService) {}
