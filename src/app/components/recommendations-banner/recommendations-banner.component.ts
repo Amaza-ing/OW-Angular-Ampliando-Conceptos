1257fcbb9100181ef0d1c137cdc79385b3cc7d00
Adrián Maza <adri_mv1@hotmail.com>
2025-03-17 16:56:57 +0100

    material components

--- src/app/components/recommendations-banner/recommendations-banner.component.ts
+++ src/app/components/recommendations-banner/recommendations-banner.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { ArticleService } from '../../services/article.service';
 import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-recommendations-banner',
-  imports: [RouterLink, MatButtonModule],
+  imports: [RouterLink, MatButtonModule, MatCardModule],
   templateUrl: './recommendations-banner.component.html',
   styleUrl: './recommendations-banner.component.css',
 })
